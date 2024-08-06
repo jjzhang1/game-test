4d88e112d6f93ec5ef6c522b384aa677b38b422d
eden.zhang <eden.zhang@bytetime.io>
2024-08-06 15:31:22 +0800

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,24 @@
 import React, { useState, useEffect } from "react";
 import styles from "@/styles/Home.module.css";
 import Head from "next/head";
-import Spline from "@splinetool/react-spline";
+import Script from "next/script";
 export default function Main() {
+  // useEffect(() => {
+  //   fetch("/index.html")
+  //     .then((response) => response.text())
+  //     .then((data) => {
+  //       document.getElementById("content").innerHTML = data;
+  //     });
+  // }, []);
+
   return (
     <>
       <Head>
         <title>Balance</title>
       </Head>
       <div style={{ width: "100%", height: "100vh", margin: 0, padding: 0 }}>
+        <div id="content"></div>
+        {/* <Script src="/index.html" strategy="beforeInteractive" /> */}
         <iframe
           src="/index.html"
           style={{
