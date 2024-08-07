06dafbc29f3893f48b509c74a15ebcba4e9fedba
eden.zhang <eden.zhang@bytetime.io>
2024-08-07 10:55:55 +0800

    test

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="script-src 'self' 'unsafe-inline'; object-src 'none';"
+        />
         <script
           async
           src="https://telegram.org/js/telegram-web-app.js"
